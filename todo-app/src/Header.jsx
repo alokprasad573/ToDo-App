5a76e0acfa9a974db7ced9bedb382a3d3341ed92
alokprasad573 <alokprasad573@outlook.com>
2025-06-04 20:59:45 +0530

    Project Name Changed.

--- todo-app/src/Header.jsx
+++ todo-app/src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return ( 
     <header className='text-white text-center mt-[50px] text-5xl font-bold'>
-        Todo App
+        TodoPie
     </header>
 );
 }
