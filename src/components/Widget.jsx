fcee4626a992a2b28fcde3e6a20c02828e2582d9
Alok Prasad <143709815+alokprasad573@users.noreply.github.com>
2025-06-06 01:39:50 +0530

    Update Widget.jsx

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -4,7 +4,7 @@ import TodoApp from './TodoApp';
 
 export default function Widget() {
     return (
-        <div className='bg-stone-300 rounded-2xl shadow-md p-4 w-fit h-fit mx-auto mt-8'>
+        <div className='bg-stone-300 rounded-2xl shadow-md p-4 w-[70vw] h-fit mx-auto mt-8'>
             <h1 className=' text-center font-bold text-2xl my-3'>Tasks</h1>
             <TodoApp></TodoApp>
         </div>
